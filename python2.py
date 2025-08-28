8e8a17f33180838642a0106e952f0ebd6c99305f
AmitKumar.Patro <AmitKumar.Patro@etplin.com>
2025-08-28 11:55:24 +0530

    commit

---
+++ python2.py
@@ -0,0 +1,9 @@
+
+num1=input('enter your data :')
+num2=input('enter your data :')
+print(f"summation is {num1+num2}")
+
+
+num1=int(input('enter your data :'))
+num2=int(input('enter your data :'))
+print(f"summation is {num1+num2}")
